152995cd09c09f46fb9f49535a99a12e8d0df00e
Shammlie <louismb@msn.com>
2025-06-02 22:33:04 +0100

    fix: update models to match better-auth schema - removes NextAuth.js specific fields and adds proper better-auth fields

---
+++ prisma/migrations/20250602213252_fix_better_auth_schema/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `provider` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerAccountId` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token_expires_in` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `type` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `sessionToken` on the `session` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "account_provider_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "session_sessionToken_key";
+
+-- AlterTable
+ALTER TABLE "account" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "provider",
+DROP COLUMN "providerAccountId",
+DROP COLUMN "refresh_token",
+DROP COLUMN "refresh_token_expires_in",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type",
+DROP COLUMN "type",
+ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "session" DROP COLUMN "expires",
+DROP COLUMN "sessionToken",
+ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "verification" ALTER COLUMN "createdAt" SET DEFAULT CURRENT_TIMESTAMP;
